5df9bc47b3ce6624b3750aa65c81452a8f16b6c6
yanghanggit <1220826563@qq.com>
2024-03-19 16:46:16 +0800

    修改严格模式

--- entitas/processors.py
+++ entitas/processors.py
@@ -1,60 +1,65 @@
-from abc import ABCMeta, abstractmethod
 
+from abc import ABCMeta, abstractmethod
 from .collector import Collector
+from .context import Context
+from .entity import Entity
+from .matcher import Matcher
+from .group import GroupEvent
+from typing import Any
 
 
 class InitializeProcessor(metaclass=ABCMeta):
     @abstractmethod
-    def initialize(self):
+    def initialize(self) -> None:
         pass
 
 
 class ExecuteProcessor(metaclass=ABCMeta):
     @abstractmethod
-    def execute(self):
+    def execute(self) -> None:
         pass
 
 
 class CleanupProcessor(metaclass=ABCMeta):
     @abstractmethod
-    def cleanup(self):
+    def cleanup(self) -> None:
         pass
 
 
 class TearDownProcessor(metaclass=ABCMeta):
     @abstractmethod
-    def tear_down(self):
+    def tear_down(self) -> None:
         pass
 
 
 class ReactiveProcessor(ExecuteProcessor):
 
-    def __init__(self, context):
+    def __init__(self, context: Context) -> None:
         self._collector = self._get_collector(context)
-        self._buffer = []
+        self._buffer: list[Entity] = []
 
     @abstractmethod
-    def get_trigger(self):
+    def get_trigger(self) -> dict[Matcher, GroupEvent]:
         pass
 
     @abstractmethod
-    def filter(self, entity):
+    def filter(self, entity: Entity) -> bool:
         pass
 
     @abstractmethod
-    def react(self, entities):
+    def react(self, entities: list[Entity]) -> None:
         pass
 
-    def activate(self):
+    def activate(self) -> None:
         self._collector.activate()
 
-    def deactivate(self):
+    def deactivate(self) -> None:
         self._collector.deactivate()
 
-    def clear(self):
+    def clear(self) -> None:
         self._collector.clear_collected_entities()
 
-    def execute(self):
+    def execute(self) -> None:
         if self._collector.collected_entities:
             for entity in self._collector.collected_entities:
                 if self.filter(entity):
@@ -66,7 +71,7 @@ class ReactiveProcessor(ExecuteProcessor):
                 self.react(self._buffer)
                 self._buffer.clear()
 
-    def _get_collector(self, context):
+    def _get_collector(self, context: Context) -> Collector:
         trigger = self.get_trigger()
         collector = Collector()
 
@@ -81,13 +86,13 @@ class ReactiveProcessor(ExecuteProcessor):
 class Processors(InitializeProcessor, ExecuteProcessor,
                  CleanupProcessor, TearDownProcessor):
 
-    def __init__(self):
-        self._initialize_processors = []
-        self._execute_processors = []
-        self._cleanup_processors = []
-        self._tear_down_processors = []
+    def __init__(self) -> None:
+        self._initialize_processors: list[InitializeProcessor] = []
+        self._execute_processors: list[ExecuteProcessor] = []
+        self._cleanup_processors: list[CleanupProcessor] = []
+        self._tear_down_processors: list[TearDownProcessor] = []
 
-    def add(self, processor):
+    def add(self, processor: Any) -> None:
         if isinstance(processor, InitializeProcessor):
             self._initialize_processors.append(processor)
 
@@ -100,23 +105,23 @@ class Processors(InitializeProcessor, ExecuteProcessor,
         if isinstance(processor, TearDownProcessor):
             self._tear_down_processors.append(processor)
 
-    def initialize(self):
+    def initialize(self) -> None:
         for processor in self._initialize_processors:
             processor.initialize()
 
-    def execute(self):
+    def execute(self) -> None:
         for processor in self._execute_processors:
             processor.execute()
 
-    def cleanup(self):
+    def cleanup(self) -> None:
         for processor in self._cleanup_processors:
             processor.cleanup()
 
-    def tear_down(self):
+    def tear_down(self) -> None:
         for processor in self._tear_down_processors:
             processor.tear_down()
 
-    def activate_reactive_processors(self):
+    def activate_reactive_processors(self) -> None:
         for processor in self._execute_processors:
             if isinstance(processor, ReactiveProcessor):
                 processor.activate()
@@ -124,15 +129,15 @@ class Processors(InitializeProcessor, ExecuteProcessor,
             if isinstance(processor, Processors):
                 processor.activate_reactive_processors()
 
-    def deactivate_reactive_processors(self):
+    def deactivate_reactive_processors(self) -> None:
         for processor in self._execute_processors:
-            if issubclass(processor, ReactiveProcessor):
+            if isinstance(processor, ReactiveProcessor):
                 processor.deactivate()
 
             if isinstance(processor, Processors):
                 processor.deactivate_reactive_processors()
 
-    def clear_reactive_processors(self):
+    def clear_reactive_processors(self) -> None:
         for processor in self._execute_processors:
             # if issubclass(type(processor), ReactiveProcessor):
             #     processor.clear()
